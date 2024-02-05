4686412c44151a8b34c2ef63c8d8c0f6ec73137a
Shanu Tiwari <shanutiwari.work@gmail.com>
2024-02-05 23:14:25 +0530

    react props

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,18 @@
+
+import Card from './components/Card'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
+       <Card username= "chaiOrcode" btnText="click me"  />
+       <Card username="shanu" btnText= "visit me" />
+
+
+    </>
+  )
+}
+
+export default App
