beff3468be7632aa5fafe2b211a2c51d6bf89376
Shanu Tiwari <shanutiwari.work@gmail.com>
2024-02-05 19:17:57 +0530

    useState hook

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,33 @@
+
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+
+  let [counter,setCounter] = useState(5)
+
+  // let counter = 5;
+
+  const addValue = ()=>{
+    
+    counter = counter+1;
+    setCounter(counter)
+  }
+
+  const removeValue = ()=>{
+    counter = counter-1;
+    setCounter(counter)
+
+  }
+  return (
+    <>
+      <h1>Chai or React</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={addValue}>Add value  {counter}</button><br />
+      <button onClick={removeValue}>remove value  {counter}</button>
+    </>
+  )
+}
+
+export default App
