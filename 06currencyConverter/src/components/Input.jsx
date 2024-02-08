53302ad71bc886cdd17741f84f9c5f3b408887cb
Shanu Tiwari <shanutiwari.work@gmail.com>
2024-02-08 14:47:49 +0530

    created a currency converter project

---
+++ 06currencyConverter/src/components/Input.jsx
@@ -0,0 +1,58 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React, { useId } from "react";
+
+function Input({
+  label,
+  amount,
+  onAmountChange,
+  onCurrencyChange,
+  selectCurrency,
+  currencyOptions = [],
+  amountDisable = false,
+  currencyDisable = false,
+
+  className = "",
+}) {
+  const amountInputID = useId();
+  return (
+    <div className={`bg-white p-3 text-sm flex ${className}`}>
+      <div className="w-1/2">
+        <label
+          htmlFor="amountInputId"
+          className="text-black/40 mb-2 inline-block"
+        >
+          {label}
+        </label>
+        <input
+          id={amountInputID}
+          className="outline-none w-full bg-transparent py-1.5"
+          type="number"
+          placeholder="Amount"
+          disabled={amountDisable}
+          value={amount}
+          onChange={(e) =>
+            onAmountChange && onAmountChange(Number(e.target.value))
+          }
+        />
+      </div>
+      <div className="w-1/2 flex flex-wrap justify-end text-right">
+        <p className="text-black/40 mb-2 w-full"></p>
+        <select
+          className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+          value={selectCurrency}
+          onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+          disabled={currencyDisable}
+        >
+          {currencyOptions.map((currency) => (
+            <option value={currency} key={currency}>
+              {currency}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
+}
+
+export default Input;
---
+++ 06currencyConverter/src/hooks/useCurrencyinfo.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch(
+      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+    )
+      .then((res) => res.json())
+      .then((res) => setData(res[currency]));
+    console.log(data);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currency]);
+  console.log(data);
+  return data;
+}
+export default useCurrencyInfo;
